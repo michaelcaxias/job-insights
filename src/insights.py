ab26b3d66cb74e9e5737fede3dc4795c03178e88
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-05-31 17:32:52 -0300

    :sparkles: feat: replica logica de filter jobs para filter industry

--- src/insights.py
+++ src/insights.py
@@ -33,21 +33,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered_industry = []
+    for job in jobs:
+        if industry == job['industry']:
+            filtered_industry.append(job)
+    return filtered_industry
 
 
 def get_max_salary(path):
