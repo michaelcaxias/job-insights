d9f5fc75d3291f9a1e9dfa6de0a48ee0eb85df50
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-05-31 15:05:15 -0300

    :sparkles: feat: cria função para lidar com jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,15 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    all_jobs = []
+    with open(path, encoding="UTF-8") as jobs:
+        jobs_list = csv.DictReader(jobs)
+        for job in jobs_list:
+            all_jobs.append(job)
+    return all_jobs
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+print(read("src/jobs.csv"))
