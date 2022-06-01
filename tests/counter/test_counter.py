d5ad5d7e7e1e1482223e8f8cb3585a04566f51e3
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-01 19:14:18 -0300

    ✅ test: cria testes para "count_ocurrences"

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,14 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences('src/jobs.csv', 'word') == 382
+    assert count_ocurrences('src/jobs.csv', 'WoRd') == 382
+    assert count_ocurrences('src/jobs.csv', 'abelha') == 0
+    assert count_ocurrences('src/jobs.csv', 'michael') != 0
+    try:
+      assert count_ocurrences('src/jobs', 'notebook') != 0
+    except FileNotFoundError:
+      assert FileNotFoundError
+
+count_ocurrences('src/jobs.csv', 'word')
