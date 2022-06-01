77f9b8ef0527173f873542f9fd841e6205f8b4da
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-01 20:02:10 -0300

    ✅ test: cria testes para função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    mock_path = 'tests/mocks/brazilians_jobs.csv'
+    mock_jobs = read_brazilian_file(mock_path)[0]
+    assert ('title' and 'salary' and 'type') in list(mock_jobs)
+    assert ('titulo' and 'salario' and 'tipo') not in list(mock_jobs)
