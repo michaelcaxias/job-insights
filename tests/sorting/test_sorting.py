e82300839e7f2084c6ab931a5511116a473c0aef
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-01 20:37:21 -0300

    ✅ test: cria testes para função "sort_by"

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,27 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    all_jobs = read("src/jobs.csv")[:10]
+
+    sort_by(all_jobs, "max_salary")
+    assert all_jobs[0]["max_salary"] == "212901"
+
+    sort_by(all_jobs, "max_salary")
+    assert all_jobs[-1]["max_salary"] == ""
+
+    sort_by(all_jobs, "min_salary")
+    assert all_jobs[0]["min_salary"] == "20000"
+
+    sort_by(all_jobs, "min_salary")
+    assert all_jobs[-1]["min_salary"] == ""
+
+    sort_by(all_jobs, "date_posted")
+    assert all_jobs[0]["date_posted"] == "2020-05-08"
+
+    sort_by(all_jobs, "date_posted")
+    assert all_jobs[-1]["date_posted"] == "2020-04-25"
+
+
+test_sort_by_criteria()
